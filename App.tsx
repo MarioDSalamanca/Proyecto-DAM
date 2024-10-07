929736f2cc5354bc83e86fa17420d7eb83ea7ca6
MarioDSalamanca <dieguezmario07@gmail.com>
2024-10-07 13:14:23 +0200

    primer día

--- App.tsx
+++ App.tsx
@@ -1,23 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+
+// Mis componentes
+import styles from './Estilos';
+import Bienvenido from './Componentes/Bienvenido';
+
+const icono = require("./assets/icon.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Voy a empezar con mi nuevo proyecto de DAM!!!</Text>
-      <StatusBar style="light" />
+      <Image source={icono} style={{width: 100, height: 100}}/>
+      <Bienvenido />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-  }
-});
---
+++ Componentes/Bienvenido.tsx
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+import styles from '../Estilos';
+
+export default function Bienvenido() {
+    return <Text style={styles.text}>Voy a empezar con mi nuevo proyecto de DAM!!!</Text>
+}
